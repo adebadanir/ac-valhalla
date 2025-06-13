64641b1d825d8184a67e75bc9784e8386db7ed3d
adebadanir <athbadanir@gmail.com>
2025-06-13 22:29:23 +0700

    feat: button hover animation

---
+++ src/app/features/about/components/about-detail/about-detail.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-detail',
+  imports: [],
+  templateUrl: './about-detail.html',
+  styleUrl: './about-detail.scss'
+})
+export class AboutDetail {
+
+}
