6493e5b0db7cedf17941701d3049cdc057889785
adebadanir <athbadanir@gmail.com>
2025-06-14 21:30:01 +0700

    feat:a about

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Button } from './features/hero/components/button/button';
 import { Hero } from './features/hero/pages/hero/hero';
+import { About } from './features/about/pages/about/about';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Hero],
+  imports: [RouterOutlet, Hero, About],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
--- src/app/features/about/pages/about/about.ts
+++ src/app/features/about/pages/about/about.ts
@@ -1,11 +1,10 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './about.html',
-  styleUrl: './about.scss'
+  styleUrl: './about.scss',
 })
-export class About {
-
-}
+export class About {}
